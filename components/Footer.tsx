375db7529ace5670fd1ad4b0efa3b8c20e616910
Sunidhi Shende <sunidhi.shende1@gmail.com>
2022-06-18 16:01:17 +0530

    changes for WeirdMaybe

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,8 +34,15 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
-
+      <div className={styles.copyright}>Website Starter Kit: <a
+            className={styles.github}
+            href={`https://github.com/transitive-bullshit/nextjs-notion-starter-kit`}
+            title={`GitHub @transitive-bullshit/nextjs-notion-starter-kit`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub />
+          </a> </div>
       <div className={styles.settings}>
         {hasMounted && (
           <a
